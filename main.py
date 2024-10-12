d27e053970d19c0fc4cba79fd584cf31e5d4f272
skjang37 <53986192+skjang37@users.noreply.github.com>
2024-10-12 15:14:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from PIL import Image, ImageOps
 from img_classification import mri_machine_classification
 
 uploaded_file = st.file_uploader("뇌 MRI 사진을 업로드 해 주세요.", type=['jpeg', 'png', 'jpg', 'webp'])
-
+'''
 if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded MRI.', use_column_width=True)
@@ -33,6 +33,6 @@ if uploaded_file is not None:
         else:
             st.write("***결과 : MRI 스캔은 건강합니다***")
 
-
+'''
 
 st.markdown("Made by MH KIM")
